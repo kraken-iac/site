0405b12378a281d35fbbb23be1e36134f0c42547
eoinfennessy <fennessyeoin@gmail.com>
2024-04-15 16:41:13 +0100

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://kraken-iac.eoinfennessy.com',
+  site: 'http://kraken-iac.eoinfennessy.com',
+  integrations: [icon()]
 });
